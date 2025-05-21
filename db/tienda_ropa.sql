5a2867b0c8628e97e71a71ed3b51268bf35e5c52
MikeSoul09 <mikiypancho@gmail.com>
2025-05-21 14:28:05 -0600

    Add files via upload

--- db/tienda_ropa.sql
+++ db/tienda_ropa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-05-2025 a las 07:07:01
+-- Tiempo de generación: 21-05-2025 a las 22:27:50
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -43,7 +43,9 @@ CREATE TABLE `clientes` (
 INSERT INTO `clientes` (`id_cliente`, `nombre`, `direccion`, `correo`, `telefono`, `ciudad`) VALUES
 (101, 'Carlos Mendoza', 'Av. Libertad 500', 'carlos.mendoza@gmail.com', '3311112222', 'Guadalajara'),
 (102, 'Ana López', 'Calle Rosa 89', 'ana.lopez@yahoo.com', '3322223333', 'Zapopan'),
-(103, 'Laura Torres', 'Calle Lago 22', 'laura.torres@hotmail.com', '3333334444', 'Guadalajara');
+(103, 'Laura Torres', 'Calle Lago 22', 'laura.torres@hotmail.com', '3333334444', 'Guadalajara'),
+(105, 'Jose', 'Malecon', 'test@gmail.com', '3322556698', 'Tlaquepaque'),
+(106, 'Bastian', 'Flores Magon 34', 'bastian34@gmail.dom', '3355668899', 'Tonala');
 
 -- --------------------------------------------------------
 
@@ -55,22 +57,21 @@ CREATE TABLE `detalle_ventas` (
   `id_detalle` int(11) NOT NULL,
   `id_venta` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `precio_unitario` decimal(10,2) NOT NULL
+  `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `detalle_ventas`
 --
 
-INSERT INTO `detalle_ventas` (`id_detalle`, `id_venta`, `id_producto`, `cantidad`, `precio_unitario`) VALUES
-(1, 7, 1, 10, 0.00),
-(2, 8, 1, 10, 0.00),
-(3, 9, 2, 15, 0.00),
-(5, 11, 3, 20, 0.00),
-(7, 15, 1, 10, 0.00),
-(16, 24, 3, 5, 0.00),
-(17, 25, 1, 10, 0.00);
+INSERT INTO `detalle_ventas` (`id_detalle`, `id_venta`, `id_producto`, `cantidad`) VALUES
+(1, 7, 1, 10),
+(2, 8, 1, 10),
+(3, 9, 2, 15),
+(5, 11, 3, 20),
+(7, 15, 1, 10),
+(19, 27, 12, 150),
+(20, 28, 3, 10);
 
 -- --------------------------------------------------------
 
@@ -85,7 +86,7 @@ CREATE TABLE `productos` (
   `descripcion` text DEFAULT NULL,
   `precio` decimal(10,2) NOT NULL,
   `stock` int(11) NOT NULL,
-  `talla_color` varchar(100) DEFAULT NULL,
+  `talla_color` varchar(100) NOT NULL,
   `descuento` decimal(5,2) DEFAULT 0.00,
   `inicio_oferta` date DEFAULT NULL,
   `fin_oferta` date DEFAULT NULL,
@@ -97,11 +98,13 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id_producto`, `nombre`, `categoria`, `descripcion`, `precio`, `stock`, `talla_color`, `descuento`, `inicio_oferta`, `fin_oferta`, `id_proveedor`) VALUES
-(1, 'Camiseta Blanca', 'Camisetas', 'Camiseta de algodón blanca', 199.99, 90, '0', 0.00, '2025-05-01', '2025-05-31', 1),
-(2, 'Camiseta Negra', 'Camisetas', 'Camiseta casual negra', 219.99, 100, '0', 0.00, '0000-00-00', '0000-00-00', 1),
-(3, 'Pantalón Azul', 'Pantalones', 'Pantalón de mezclilla azul', 399.00, 150, '30', 0.00, '2025-05-10', '2025-05-25', 2),
-(4, 'Gorra Estilo', 'Accesorios', 'Gorra con diseño urbano', 150.50, 200, '0', 0.00, '0000-00-00', '0000-00-00', 3),
-(5, 'Cinturón Cuero', 'Accesorios', 'Cinturón de cuero genuino', 299.90, 150, '0', 15.00, '2025-05-05', '2025-06-01', 3);
+(1, 'Camiseta Blanca', 'Camisetas', 'Camiseta de algodón blanca', 199.99, 90, 'S,M,L,XL', 20.00, '2025-05-01', '2025-05-31', 1),
+(2, 'Camiseta Negra', 'Camisetas', 'Camiseta casual negra', 219.99, 100, 'S,M,L,XL', 0.00, '0000-00-00', '0000-00-00', 1),
+(3, 'Pantalón Azul', 'Pantalones', 'Pantalón de mezclilla azul', 399.00, 140, '30,31,32', 30.00, '2025-05-10', '2025-05-25', 2),
+(4, 'Gorra Estilo', 'Accesorios', 'Gorra con diseño urbano', 150.50, 200, 'Unitalla', 0.00, '0000-00-00', '0000-00-00', 3),
+(5, 'Cinturón Cuero', 'Accesorios', 'Cinturón de cuero genuino', 299.90, 150, '28,30,32', 15.00, '2025-05-05', '2025-06-01', 3),
+(12, 'Pantalón Negro', 'Pantalones', 'Mezclilla negra', 399.00, 0, '30,32,36,38', 10.00, '2025-05-18', '2025-05-25', 2),
+(13, 'Camiseta Gris', 'Camisetas', 'Camiseta de algodón gris', 200.00, 150, 'S,M,L', 10.00, '2025-05-17', '2025-05-24', 1);
 
 -- --------------------------------------------------------
 
@@ -124,7 +127,10 @@ CREATE TABLE `proveedores` (
 INSERT INTO `proveedores` (`id_proveedor`, `nombre`, `direccion`, `correo`, `telefono`) VALUES
 (1, 'Proveedora Moda SA', 'Av. Central 123', 'contacto@modasa.com', '3312345678'),
 (2, 'Textiles MX', 'Calle Diseño 456', 'ventas@textilesmx.com', '3322334455'),
-(3, 'Accesorios Fashion', 'Plaza Ropa Local 9', 'info@accesfashion.com', '3344556677');
+(3, 'Accesorios Fashion', 'Plaza Ropa Local 9', 'info@accesfashion.com', '3344556677'),
+(4, 'Jose', 'MAlecon', 'test@gmail.com', '3322556688'),
+(5, 'Marco García', 'Donato Guerra 200', 'MGarcia@hotmail.com', '344778899'),
+(6, 'Pedro Picapiedra', 'Valle del dino', 'picapiedra2000@gmail.com', '0');
 
 -- --------------------------------------------------------
 
@@ -146,9 +152,12 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `password`, `rol`) VALUES
 (7, 'MIguel Isaac', 'Migueles1009', '$2y$10$P.XtY9YDcsHb9mfs.RInlurrgZjeSa9Y0Il.gr2jWSI4JK/OgkCUm', 'vendedor'),
-(8, 'Jose', 'Peputo', '$2y$10$psFa2Sfk0rkMWUov9piGR./BeFw6NgEOsJeqzpe0ybMcr9aF4vJsO', 'admin'),
+(8, 'Admin', 'Admin', '$2y$10$psFa2Sfk0rkMWUov9piGR./BeFw6NgEOsJeqzpe0ybMcr9aF4vJsO', 'admin'),
 (9, 'User1', 'User1', '$2y$10$ZwzXloQj7ualGugv.e/y6eAKxY4GtoYebljd510qhk1LbXBmOghVK', 'vendedor'),
-(10, 'Isaac', 'Isaquesedeaqui', '$2y$10$IpF3C8l4DFVrZlZlNQmsb.yjfNXPZ.Ld7/YtOnMqis6GzeCkrjHym', 'admin');
+(10, 'Isaac', 'Isaquesedeaqui', '$2y$10$IpF3C8l4DFVrZlZlNQmsb.yjfNXPZ.Ld7/YtOnMqis6GzeCkrjHym', 'admin'),
+(11, 'Vendedor14', 'Vend14', '$2y$10$v6pGM1a/ir.gCRjvBIUN9e1vSMlBZiigLJRh0Z6Y0lrG024XMFV36', 'vendedor'),
+(13, 'Vendedor14', 'Vend15', '$2y$10$va81ndODWurHf9hDpRsdXOv.IsqJMEQ2Lh6zVGJUNvhIKxHSvhRb2', 'vendedor'),
+(14, 'Pepe', 'vend16', '$2y$10$NiE41NF5utsTCGANErHSPO6FD8X14oM83WJcPyiOYqAQgtKmXGpJi', 'vendedor');
 
 -- --------------------------------------------------------
 
@@ -179,12 +188,12 @@ INSERT INTO `ventas` (`id_venta`, `id_cliente`, `fecha`, `metodo_pago`) VALUES
 (9, 102, '2025-05-19 15:09:50', 'Efectivo'),
 (11, 103, '2025-05-19 15:10:56', 'Efectivo'),
 (13, 103, '2025-05-19 22:17:50', 'Efectivo'),
-(14, 103, '2025-05-19 22:18:28', 'Efectivo'),
 (15, 103, '2025-05-19 22:19:11', 'Efectivo'),
-(20, 103, '2025-05-20 00:00:00', 'Efectivo'),
-(21, 103, '2025-05-19 22:34:42', 'Efectivo'),
 (24, 102, '2025-05-19 22:47:49', 'Efectivo'),
-(25, 101, '2025-05-19 23:04:42', 'Tarjeta');
+(25, 101, '2025-05-19 23:04:42', 'Tarjeta'),
+(26, 101, '2025-05-20 21:29:48', 'Efectivo'),
+(27, 101, '2025-05-20 22:08:11', 'Transferencia'),
+(28, 106, '2025-05-20 22:20:24', 'Tarjeta');
 
 --
 -- Índices para tablas volcadas
@@ -242,37 +251,37 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_ventas`
 --
 ALTER TABLE `detalle_ventas`
-  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
-  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Restricciones para tablas volcadas
